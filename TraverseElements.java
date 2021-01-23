4491aa62248f54b2efc347fe0e4923fec5feefef
adam's <asandler778@gmail.com>
2021-01-23 22:04:47 +0300

    changes on traverse algorithm

---
+++ TraverseElements.java
@@ -0,0 +1,10 @@
+public class TraverseElements { 
+   public static void main(String[] args) { 
+	int arr[]={1,2,3,4,5}; 
+	int length= arr.length; 	
+	System.out.println("Array Elements are:-"); 	
+	for(int i=0;  i<length;  i++) { 					     
+  System.out.print(arr[i]+" "); 
+	} 
+    } 
+}
